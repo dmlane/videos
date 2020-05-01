57b46c7817c351939736743f639a059e3cb7d65f
David Lane <git2018@dmlane.net>
2020-05-01 10:39:07 +0200

    Perms test

--- mp4_length.pl
+++ mp4_length.pl
@@ -4,13 +4,10 @@ use MP4::Info;
 use Term::ReadKey;
 use Switch;
 
-my ($fn)=@ARGV;
+my ($fn) = @ARGV;
 
-
-	my $info=get_mp4info($fn);
-	my $vhours=int($info->{MM}/60);
-	my $vmins=int($info->{MM}%60);
-	printf("%02d:%02d:%02d.%003d\n",$vhours,$vmins,$info->{SS},$info->{MS});
-
-	
+my $info   = get_mp4info($fn);
+my $vhours = int( $info->{MM} / 60 );
+my $vmins  = int( $info->{MM} % 60 );
+printf( "%02d:%02d:%02d.%003d\n", $vhours, $vmins, $info->{SS}, $info->{MS} );
 
