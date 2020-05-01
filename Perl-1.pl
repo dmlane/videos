ceef6652941724028eaf20a4f21eb56a60489958
David Lane <git2018@dmlane.net>
2020-05-01 07:51:01 +0200

    test

--- Perl-1.pl
+++ Perl-1.pl
@@ -2,30 +2,18 @@
 #use Term::ANSIScreen  qw/:color :cursor :screen :keyboard/;
 use strict;
 
-
-
-
-
-
-
-
-
-
-
-#
+##
 use Clipboard;
 Clipboard->copy('#');
 my $res     = "#";
 my $default = "01:02:03.004";
 
-sub tsktsk
-{
+sub tsktsk {
     $SIG{INT} = \&tsktsk;
     Clipboard->copy($default);
 }
 $SIG{INT} = \&tsktsk;
-until ( $res =~ /^\d\d:\d\d:\d\d\.\d\d\d/ )
-{
+until ( $res =~ /^\d\d:\d\d:\d\d\.\d\d\d/ ) {
     sleep(1);
     $res = Clipboard->paste;
 }
